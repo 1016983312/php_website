eec65943809e9fc28d2bdfd86f4f12b9ead2dd62
Richardy <1016983312@qq.com>
2020-05-14 16:50:32 +0800

    +menu.php
    +footer.php

---
+++ components/footer.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>Copyright © 2019-" . date("Y") . " ASRIBS.com</p>";
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ASRIBS</title>
+</head>
+
+<body>
+    <div class="menu">
+        <?php include 'components/navigator.php';?>
+    </div>
+    <div>
+        hello world!
+    </div>
+    <div class="footer">
+        <?php include 'components/footer.php';?>
+    </div>
+
+</body>
+
+</html>
