62f9e1fde0d01815be1587143843da2482394771
you.jj <1016983312@qq.com>
2020-05-20 23:09:44 +0800

    create logo & style

--- components/navigator.php
+++ components/navigator.php
@@ -1,30 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="/css/default.css">
     <link rel="stylesheet" type="text/css" href="/css/navigator.css">
 </head>
+
 <body>
     <div id="menu">
-    <ul>
-        <li><a href="/index.php" class="logo" ></a></li>
-        <li> <a href="/index.php" class="nodrop">首页</a></li>
-        <li><a href="" class="drop">常用工具</a>
-        <div class="dropdown_1column">
-            <div class="col_1">
-                    <ul class="simple">
-                            <li><a href="/pages/tools/dateCalc.php">日期计算器</a></li>
-                            <li><a href="">导航条代码</a></li>
-                            <li><a href="">电子商务</a></li>
-                    </ul>
-            </div>
-       
+        <div class="divlogo m-left-45 m-right-10"><a href="/index.php" class="logo" title="asribs"><img src="/images/Asribs.png"
+                    alt="asribs"></a>
         </div>
-        </li>
-        <li><a href="" class="nodrop">联系我</a></li>
-    </ul>
+        <div class="divnav">
+            <ul>
+                <li> <a href="/index.php" class="nodrop">首页</a></li>
+                <li><a href="" class="drop">常用工具</a>
+                    <div class="dropdown_1column">
+                        <div class="col_1">
+                            <ul class="simple">
+                                <li><a href="/pages/tools/dateCalc.php">日期计算器</a></li>
+                                <li><a href="">导航条代码</a></li>
+                                <li><a href="">电子商务</a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </li>
+                <li><a href="" class="nodrop">联系我</a></li>
+            </ul>
+        </div>
     </div>
 </body>
 <script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.5.0.min.js"></script>
+
 </html>
