95b8ad298f3ff3c9e788f37bf6e1f64647313485
jjyou <1016983312@qq.com>
2020-09-28 10:38:34 +0800

    add contact us page and css

--- components/navigator.php
+++ components/navigator.php
@@ -20,26 +20,28 @@
 
 <body>
     <div id="menu">
-        <div class="divlogo m-left-45 m-right-10"><a href="/index.php" class="logo" title="asribs"><img
-                    src="/images/Asribs.png" alt="asribs"></a>
-        </div>
-        <div class="divnav">
-            <ul>
-                <li> <a href="/index.php" class="nodrop">首页</a></li>
-                <li><a href="" class="drop">常用工具</a>
-                    <div class="dropdown_1column">
-                        <div class="col_1">
-                            <ul class="simple">
-                                <li><a href="/pages/tools/dateCalc.php">日期计算器</a></li>
-                                <li><a href="">导航条代码</a></li>
-                                <li><a href="">电子商务</a></li>
-                            </ul>
-                        </div>
+        <div id="menu-item">
+            <div class="divlogo m-left-45 m-right-10"><a href="/index.php" class="logo" title="asribs"><img
+                        src="/images/Asribs.png" alt="asribs"></a>
+            </div>
+            <div class="divnav">
+                <ul>
+                    <li> <a href="/index.php" class="nodrop">首页</a></li>
+                    <li><a href="" class="drop">常用工具</a>
+                        <div class="dropdown_1column">
+                            <div class="col_1">
+                                <ul class="simple">
+                                    <li><a href="/pages/tools/dateCalc.php">日期计算器</a></li>
+                                    <li><a href="">导航条代码</a></li>
+                                    <li><a href="">电子商务</a></li>
+                                </ul>
+                            </div>
 
-                    </div>
-                </li>
-                <li><a href="" class="nodrop">联系我</a></li>
-            </ul>
+                        </div>
+                    </li>
+                    <li><a href="/pages/contactus/contactus.php" class="nodrop">联系我</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </body>
---
+++ pages/contactus/contactus.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>联系我们</title>
+    <link rel="stylesheet" type="text/css" href="css/contactus.css">
+</head>
+
+<body>
+    <div class="menu">
+        <?php include '../../components/navigator.php';?>
+    </div>
+    <div id="" class="content">
+        
+    </div>
+</body>
+<script type="text/javascript">
+
+</script>
+
+</html>
