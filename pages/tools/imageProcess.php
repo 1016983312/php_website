265ad9c6d6624d993769db9c1bd16e98450828fc
jjyou <1016983312@qq.com>
2021-06-07 17:49:44 +0800

    add file uploader plug-in
    http://fex.baidu.com/webuploader/

---
+++ pages/tools/imageProcess.php
@@ -0,0 +1,10 @@
+<?php  
+require_once 'AipImageProcess.php';
+
+// 你的 APPID AK SK
+const APP_ID = '24327231';
+const API_KEY = 'gyGimPqi9Y0Pk5loAii9XMyb';
+const SECRET_KEY = 'gr1KWSdRzBtq4sb8OTeGL247IMmlLTYu';
+
+$client = new AipImageProcess(APP_ID, API_KEY, SECRET_KEY);
+?>
