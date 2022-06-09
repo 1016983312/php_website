d68d761d4de2842d6a43828412cd6626fb2728bd
1016983312 <1016983312@qq.com>
2022-06-09 16:30:16 +0800

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,3 @@
 <?php
-echo "<p>Copyright © 2019-" . date("Y") . " ASRIBS.com <br/><a target=\"_blank\" href=\"http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31010702007114\" >沪公网安备 31010702007114号</a><br/><a target=\"_blank\" href=\"https://beian.miit.gov.cn/\" rel=\"noopener noreferrer\">沪ICP备2021013905号-1</a></p>";
+echo "<p style=\"text-align:center;\">Copyright © 2019-" . date("Y") . " ASRIBS.com <br/><a target=\"_blank\" href=\"http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31010702007114\" >沪公网安备 31010702007114号</a><br/><a target=\"_blank\" href=\"https://beian.miit.gov.cn/\" rel=\"noopener noreferrer\">沪ICP备2021013905号-1</a></p>";
 ?>
